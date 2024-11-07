07319e10974fa1db13f10d04a3e7599a2f3499d8
Jungyu Park <parkjungyu09@gmail.com>
2024-11-07 23:23:22 +0900

    feat: task 추가 기능

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Dropdown, DropdownButton, Form, Check } from "react-bootstrap";
+import {
+  Button,
+  Dropdown,
+  DropdownButton,
+  Form,
+  Check,
+  InputGroup,
+  FloatingLabel,
+} from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faPen } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
 
 function App() {
+  let [task, setTask] = useState([]);
   return (
     <div className="App">
       <h1 className="logo-text">TODO LIST</h1>
       <div className="top-nav">
-        <Button id="addTask-button" variant="primary">
-          Add Task
-        </Button>
+        <InputGroup className="mb-3">
+          <InputGroup.Text id="inputGroup-sizing-default">
+            Add Task
+          </InputGroup.Text>
+          <Form.Control
+            onKeyDown={(e) => {
+              if (e.key == "Enter" && e.nativeEvent.isComposing == false) {
+                let copy = [...task];
+                copy.push(e.target.value);
+                setTask(copy);
+                e.target.value = "";
+                console.log(task);
+              }
+            }}
+            aria-label="Default"
+            aria-describedby="inputGroup-sizing-default"
+          />
+        </InputGroup>
         <DropdownButton id="dropdown-basic-button" title="All">
           <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
           <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
@@ -19,37 +44,53 @@ function App() {
         </DropdownButton>
       </div>
       <container className="todo-list">
-        <div className="list-box">
-          <div style={{ display: "flex", gap: "12px" }}>
-            <Form.Check style={{ scale: "1.4" }} aria-label="option 1" />
-            안녕
-          </div>
-          <div
-            style={{ display: "flex", gap: "8px"}}
-          >
-            <Button id="color-ligthgray">
-              <FontAwesomeIcon icon={faTrash} />
-            </Button>
-            <Button id="color-ligthgray">
-              <FontAwesomeIcon icon={faPen} />
-            </Button>
-          </div>
-        </div>
-        <div></div>
-
-        <div className="list-box">안녕</div>
-        <div className="list-box">안녕</div>
-        <div className="list-box">안녕</div>
+        {task.map((a, i) => {
+          return <ListBox task={task} i={i} setTask={setTask} />;
+        })}
       </container>
-      <container></container>
     </div>
   );
 }
 
-function todoList() {
-  {
-    <div className="list-box"></div>;
-  }
+function ListBox(props) {
+  let [isEditing, setIsEditing] = useState(false);
+  let [edit, setEdit] = useState(props.task[props.i]);
+
+  return (
+    <div className="list-box">
+      <div style={{ display: "flex", gap: "12px" }}>
+        <Form.Check style={{ scale: "1.4" }} aria-label="option 1" />
+        {isEditing == false ? (
+          <span>{props.task[props.i]}</span>
+        ) : (
+          <FloatingLabel controlId="floatingInput"  className="mb-3">
+            <Form.Control id="edit-input" type="text" placeholder="" />
+          </FloatingLabel>
+        )}
+      </div>
+
+      <div style={{ display: "flex", gap: "8px" }}>
+        <Button
+          onClick={() => {
+            let copy = [...props.task];
+            copy.splice(props.i, 1);
+            props.setTask(copy);
+          }}
+          id="color-ligthgray"
+        >
+          <FontAwesomeIcon icon={faTrash} />
+        </Button>
+        <Button
+          onClick={() => {
+            setIsEditing(!isEditing);
+          }}
+          id="color-ligthgray"
+        >
+          <FontAwesomeIcon icon={faPen} />
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
